be7bb286f30efa513f889c14d86403a0ae6638c8
Christopher <christopherhenryjacob@gmail.com>
2022-10-31 18:28:00 +0530

    routes added for employee components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { EmployeeFormComponent } from './pages/employee-form/employee-form.component';
+import { EmployeeListComponent } from './pages/employee-list/employee-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'emp-form', component: EmployeeFormComponent },
+  { path: 'emp-list', component: EmployeeListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
