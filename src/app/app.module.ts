0204763a8099ff8b767b8beeba519ef18949b50d
Christopher <christopherhenryjacob@gmail.com>
2022-10-31 18:16:05 +0530

    Added employee form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmployeeFormComponent } from './pages/employee-form/employee-form.component';
 
 registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeFormComponent
   ],
   imports: [
     BrowserModule,
